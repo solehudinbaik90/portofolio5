93f12c041be268e4d2b5aad9cca55733ef2f8cd0
Muhamad Soleh <solehudinz3692@gmail.com>
2026-07-17 20:30:56 +0700

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        theme: "rgb(255 1 79)",
+        card: "rgb(29 28 34)",
+        btn: "rgb(41 40 45)",
+        text: "rgb(196 207 222)",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        heading: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
