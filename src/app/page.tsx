ffd6c73e69822680d2b67c1bbb9a907f80429a22
Muhamad Soleh <solehudinz3692@gmail.com>
2026-07-18 14:40:49 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,18 @@ import Footer from "@/components/layout/Footer";
 import Hero from "@/components/home/Hero";
 import ProfileImage from "@/components/home/ProfileImage";
 
-export const metadata: Metadata = { title: "Beranda" };
+export const metadata: Metadata = {
+  title: "Beranda",
+  description: "Menampilkan Profil, CV, Resume, dan Proyek dari Muhamad Soleh.",
+};
 
 export default function HomePage() {
   return (
-    <div className="bg-img">
-      <Header />
-      <section className="relative z-[99]">
-        <div className="main__body">
+    <>
+      <div className="bg-img" />
+      <div className="relative z-[10] min-h-screen">
+        <Header />
+        <main className="relative z-[99]">
           <section className="py-[30px] md:py-[80px]">
             <div className="container mx-auto">
               <div className="grid grid-cols-12 gap-[30px]">
@@ -20,9 +24,9 @@ export default function HomePage() {
               </div>
             </div>
           </section>
-        </div>
-      </section>
-      <Footer />
-    </div>
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
