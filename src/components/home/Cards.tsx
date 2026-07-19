701f9520dc111ea7cc51a47b7ad63486d9e6284a
Muhamad Soleh <solehudinz3692@gmail.com>
2026-07-19 22:03:44 +0700

    Update Cards.tsx

--- src/components/home/Cards.tsx
+++ src/components/home/Cards.tsx
@@ -4,7 +4,7 @@ export default function Cards() {
   return (
     <div className="mt-[60px] grid grid-cols-12 gap-[30px]">
       <div className="col-span-12 md:col-span-4" data-aos="fade-up" data-aos-delay="350">
-        <div className="border-text grid gap-[30px] rounded-lg border-2 border-dotted p-[20px]">
+        <div className="border-text grid gap-[38px] rounded-lg border-2 border-dotted p-[20px]">
           <h3 className="text-center text-[24px]">Layanan</h3>
           <div className="flex justify-around p-[10px]">
             <Image src="/assets/images/icon/icon-2.png" alt="Ikon Layanan 1" />
@@ -15,9 +15,9 @@ export default function Cards() {
       </div>
 
       <div className="col-span-12 md:col-span-4" data-aos="fade-up" data-aos-delay="350">
-        <div className="border-text grid gap-[10px] rounded-lg border-2 border-dotted p-[20px]">
+        <div className="border-text grid gap-[38px] rounded-lg border-2 border-dotted p-[20px]">
           <h3 className="text-center text-[24px]">Proyek</h3>
-          <div className="flex justify-around">
+          <div className="flex justify-around p-[10px]">
             <Image src="/assets/images/show/show-1.png" alt="Tampilan Proyek" />
           </div>
         </div>
