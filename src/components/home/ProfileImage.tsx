5741d2ac1a020b3299162143721d44dd653977f8
Muhamad Soleh <solehudinz3692@gmail.com>
2026-07-19 14:09:14 +0700

    Update ProfileImage.tsx

--- src/components/home/ProfileImage.tsx
+++ src/components/home/ProfileImage.tsx
@@ -30,7 +30,7 @@ export default function ProfileImage() {
         </div>
 
         <div
-          className="pulse bg-card absolute bottom-[-40px] left-1/2 -translate-x-1/2 flex items-center gap-[20px] rounded-full px-[40px] py-[18px] md:bottom-[-20px] lg:bottom-[-30px]"
+          className="pulse bg-card absolute bottom-[-50px] right-[40px] flex items-center gap-[20px] rounded-full px-[40px] py-[18px] md:bottom-[-17px] lg:right-[-34px]"
           data-aos="fade-up"
           data-aos-delay="350"
         >
@@ -43,7 +43,7 @@ export default function ProfileImage() {
             <path d="M512 32c0 113.6-84.6 207.5-194.2 222c-7.1-53.4-30.6-101.6-65.3-139.3C290.8 46.3 364 0 448 0h32c17.7 0 32 14.3 32 32zM0 96C0 78.3 14.3 64 32 64H64c123.7 0 224 100.3 224 224v32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V320C100.3 320 0 219.7 0 96z" />
           </svg>
           <div>
-            <h2 className="text-[26px] font-semibold">1.1rb+</h2>
+            <h2 className="text-[26px] font-semibold">100.000K+</h2>
             <p className="text-base text-white">Pelanggan Setia</p>
           </div>
         </div>
